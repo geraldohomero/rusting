86f36977d5c430fd45ffc8a24d4b5a0b0adfdad5
geraldohomero <geraldohomero@protonmail.com>
2024-02-27 18:45:39 -0300

    update

--- x3-shadowing-constants/src/main.rs
+++ x3-shadowing-constants/src/main.rs
@@ -24,4 +24,42 @@ fn main() {
     let s = 5*5;
 
     println!("The value of s is: {}", s); // 25
+
+    let mut p = 5;
+    let p = p * 2;
+    // p = 60; // cannot assign twice to immutable variable
+
+    //shadowing its type
+
+    let q = 32;
+    let q = "thirty two";
+    println!("The value of q is: {}", q); // thirty two
+    // this can be done multiple times
+
+    // the scope of the shadowed variable is limited to the block in which it is declared
+    let mut r = 65;
+    {
+        let r = 32;
+        println!("The value of r inside the block is: {}", r); // 32
+    }
+    println!("The value of r outside the block is: {}", r); // 65
+    // but if we remove the let keyword from the r inside the block, the value of r outside the block will be 32
+    // because the r inside the block is a new variable that shadows the r outside the block
+    let mut r = 65;
+    {
+        r = 32; // let removed
+        println!("2 The value of r inside the block is: {}", r); // 32
+    }
+    println!("2 The value of r outside the block is: {}", r); // 32
+
+    // Constants
+    // are always immutable
+    // Difference between constants and immutable variables
+    //          1. You are not allowed to use mut with constants, they [constants] are always immutable
+    //          2. You will declare a constant using the const keyword instead of the let keyword and the type of the value must be annotated
+    //             The compiler will NOT infer and add the type of the value for you
+
+    const MAX_POINTS: u32 = 100_000; // the type of the value must be annotated
+                                     // the name of the constant must be in uppercase
+
 }
